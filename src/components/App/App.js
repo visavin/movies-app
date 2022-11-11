09b59a2d2f03452b211f7c45727c281a6ae26fae
visavin <visavin@yandex.ru>
2022-11-11 21:35:51 +0300

    Movies App - Лоадер и обработка ошибок

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
-import { Component } from 'react'
-// import { Spin } from 'antd'
+import React, { Component } from 'react'
+import { Spin, Alert } from 'antd'
+import { Offline } from 'react-detect-offline'
 
 import FilmList from '../FilmList'
 import ThemoviedbService from '../../services/ThemoviedbService'
@@ -12,6 +13,8 @@ export default class App extends Component {
 
   state = {
     movies: [{ id: 0 }],
+    loading: true,
+    error: false,
   }
 
   constructor(props) {
@@ -20,20 +23,55 @@ export default class App extends Component {
   }
 
   onMoviesLoaded = (movies) => {
-    this.setState({ movies })
+    this.setState({ movies, loading: false })
+  }
+
+  onError = () => {
+    this.setState({ error: true, loading: false })
   }
 
   updateMovies() {
-    this.movieService.searchFilms('return').then(this.onMoviesLoaded)
+    this.movieService.searchFilms('return').then(this.onMoviesLoaded).catch(this.onError)
   }
 
   render() {
-    const { movies } = this.state
+    const { movies, loading, error } = this.state
+    const hasData = !(loading || error)
+    const errorMessage = error ? (
+      <Alert
+        message="Error"
+        description="This is an error message about connection with database."
+        type="error"
+        showIcon
+      />
+    ) : null
+    const spinner = loading ? <Spin size="large" /> : null
+    const content = hasData ? <FilmList movies={movies} /> : null
 
     return (
       <div className="container">
-        <FilmList movies={movies} />
+        <Offline>
+          <OfflineAlert />
+        </Offline>
+        {errorMessage}
+        {spinner}
+        {content}
       </div>
     )
   }
+}
+
+const OfflineAlert = () => {
+  return (
+    <React.Fragment>
+      <div className="overlay"></div>
+      <Alert
+        className="alert"
+        message="Warning"
+        description="You do not have an Internet connection."
+        type="warning"
+        showIcon
+      />
+    </React.Fragment>
+  )
 }
